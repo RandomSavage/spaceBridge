43c93225c19dbd66067afba32c9696a4e92bb8aa
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-07-01 22:52:23 -0400

    Building Content Section Component Template

---
+++ components/UI/Content/Content.js
@@ -0,0 +1,16 @@
+const Content = () => {
+  return (
+    <div className="content-area">
+      <img src="https://i.pinimg.com/originals/78/53/ec/7853ecc94a462304864cddd8c311d8e4.jpg" alt="model 500 telephone" />
+      <aside className="content-area__inner">
+        <h2>Sed gitsum Dolor</h2>
+        <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris,<br />eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+        <button className="content-area__actionBtn">
+          <a href="/" >Learn More</a>
+        </button>
+      </aside>
+    </div>
+  )
+}
+
+export default Content
--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,15 @@
 import MainLayout from '../components/layouts/MainLayout'
 import SideNav from '../components/UI/SideNav/SideNav'
 import Landing from '../components/UI/Landing/Landing'
-
+import Content from '../components/UI/Content/Content'
 
 const HomeView = () => {
   return (
     <MainLayout>
     <div className="landing-full">
       <SideNav />
-      <Landing />
+      {/* <Landing /> */}
+      <Content />
     </div>
   </MainLayout>
   )
