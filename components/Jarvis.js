41ec0b381d41265d085e978fb37f53a9caa197a1
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-07-04 01:18:03 -0400

    looping Data for Content Sections from useContextoriginating in Jarvis.js and styling

--- components/Jarvis.js
+++ components/Jarvis.js
@@ -15,17 +15,26 @@ const[myContent, setMyContent] = useState(
     {
       heading: 'heading 1',
       paragraph: 'Paragraph 1',
-      bgColor: '#1b1b1b'
+      bgColor: '#5052b5',
+      screen: '.8',
+      image: 'https://i.pinimg.com/originals/78/53/ec/7853ecc94a462304864cddd8c311d8e4.jpg',
+      alt: "Model 500 Telephone"
     },
     {
       heading: 'heading 2',
       paragraph: 'Paragraph 2',
-      bgColor: '#303030'
+      bgColor: '##b74e91',
+      screen: '.9',
+      image: 'https://cdn.shopify.com/s/files/1/0149/4302/products/telephone_wire_rainbow_1200x1200.jpg?v=1571262738',
+      alt: "African Woven Basket Fabricated from telephone wires"
     },
     {
       heading: 'heading 3',
       paragraph: 'Paragraph 3',
-      bgColor: '#fff'
+      bgColor: '5e42a6',
+      screen: '1',
+      image: 'https://i.pinimg.com/originals/d9/05/c8/d905c8230817bea67982718d9def02bc.jpg',
+      alt: "Model in a red Telephone Booth"
     }
   ]
 )
--- components/UI/Content/Content.js
+++ components/UI/Content/Content.js
@@ -2,29 +2,38 @@ import { useStateContext } from '../../Jarvis'
 
 const loopContent = (content, num) => {
   let loopedContent = []
+  let myIndex;
   for(let i = 0; i < num; i += 1) {
     loopedContent.push(content)
   }
+  for(let j = 0; j < index; j += 1) {
+    myIndex = j
+  }
     return loopedContent
-
 }
 
 const Content = () => {
   const globalState = useStateContext()
+  console.log(globalState.myContent)
+  let loopMeBitch = globalState.myContent
   return (
     <>
-    {loopContent(
-      (<div className="content-area">
-      <img src="https://i.pinimg.com/originals/78/53/ec/7853ecc94a462304864cddd8c311d8e4.jpg" alt="model 500 telephone" />
-      <aside className="content-area__inner" style={{background: globalState.myContent.bgColor}}>
-        <h2>Sed gitsum Dolor</h2>
-        <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris,<br />eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-        <button className="content-area__actionBtn">
-          <a href="/" >Learn More</a>
-        </button>
-      </aside>
-    </div>), 3)}
-  
+    { 
+    loopMeBitch.map((obj, index) => {
+      return (
+        <div className="content-area">
+          <img src={loopMeBitch[index].image} alt={loopMeBitch[index].alt} />
+          <aside className="content-area__inner" style={{background: loopMeBitch[index].bgColor, opacity: loopMeBitch[index].screen}}>
+            <h2>Sed gitsum Dolor</h2>
+            <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris,<br />eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+            <button className="content-area__actionBtn">
+              <a href="/" >Learn More</a>
+            </button>
+          </aside>
+        </div>
+      )        
+    }) 
+  } 
   </>
   )
 }
