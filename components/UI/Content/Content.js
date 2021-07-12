177e564607fda92eb53ddc68dbfdb62a61e46bad
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-07-11 22:36:30 -0400

    Mobile Styling and Code Cleaning on The Scss files

--- components/UI/Content/Content.js
+++ components/UI/Content/Content.js
@@ -17,24 +17,24 @@ const Content = () => {
   console.log(globalState.myContent)
   let loopMeBitch = globalState.myContent
   return (
-    <>
+    <div className="content-container">
     { 
     loopMeBitch.map((obj, index) => {
       return (
         <div className="content-area">
           <img src={loopMeBitch[index].image} alt={loopMeBitch[index].alt} />
-          <aside className="content-area__inner" style={{background: loopMeBitch[index].bgColor, opacity: loopMeBitch[index].screen}}>
+          <div className="content-area__inner" style={{background: loopMeBitch[index].bgColor, opacity: loopMeBitch[index].screen}}>
             <h2>Sed gitsum Dolor</h2>
             <p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris,<br />eu ultricies erat malesuada quis. Aliquam dapibus.</p>
             <button className="content-area__actionBtn">
               <a href="/" >Learn More</a>
             </button>
-          </aside>
+          </div>
         </div>
       )        
     }) 
   } 
-  </>
+  </div>
   )
 }
 
