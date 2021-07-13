b629d489073b4e0d74597e2d125d14f81e344950
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-07-12 23:18:09 -0400

    working on adding gsap

--- components/UI/Landing/Landing.js
+++ components/UI/Landing/Landing.js
@@ -1,13 +1,46 @@
-import { FaTimes, FaSun} from "react-icons/fa";
+import { FaTimes, FaSun} from "react-icons/fa"
+import React, { useEffect, useRef } from 'react'
+import { gsap, Power3} from 'gsap'
+// import { CSSPlugin } from 'gsap/CSSPlugin'
 import { useStateContext } from '../../Jarvis'
 
+
+
 const Landing = () => {
+  // Force CSSPlugin to not get dropped during build
   const globalState = useStateContext()
+  let landingHeader = useRef(null)
+  let mySun = useRef(null)
+  const SunIcon = React.forwardRef((props, ref) => (
+    <FaSun ref={mySun} {...props} onClick={() => globalState.setSideNavOpenAction(true)} />
+  ));
+  
+  // You can now get a ref directly to the DOM button:
+  const ref = React.createRef()
+  // const element = ref.current
+  //useEffect runs after all dom elements are loaded/returned below
+  useEffect(() => {
+    const element = ref.current
+    gsap.fromTo(
+      document.querySelectorAll('.landing__closeBtn'),
+      {
+        opacity: 0,
+        y: 200,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 8
+      },
+    )
+  }, [])
+
   return (
     <div className="landing">
       <FaTimes className="landing__closeBtn" onClick={() => globalState.setSideNavOpenAction(true)} />
-      <FaSun className="landing__closeBtn" onClick={() => globalState.setSideNavOpenAction(true)} />
-      <h1>spaceBridge</h1>
+      <SunIcon className="landing__closeBtn"  />
+      {/* <FaSun className="landing__closeBtn" onClick={() => globalState.setSideNavOpenAction(true)} /> */}
+      <h1 ref={el => {landingHeader = el}}>spaceBridge</h1>
       <p>
         Just another RandomSavage Clone produced under the influence of <a  href="https://www.youtube.com/watch?v=InGPB36CFCI" className="landing__links" target="_blank">Techno Music</a>
         <br/>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import './styles/styles.scss'
 import { Jarvis } from '../components/Jarvis'
+// import { gsap } from 'gsap'
+// import { CSSPlugin } from 'gsap/CSSPlugin'
+
+
+// // Force CSSPlugin to not get dropped during build
+// gsap.registerPlugin(CSSPlugin)
 
 function MyApp({ Component, pageProps}) {
   return(
